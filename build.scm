e5b4bfee10e6a054f12a10fb48d0542da7f91844
Mohammed Samy <43931403+Morisamy@users.noreply.github.com>
2024-08-18 10:55:37 +0300

    update version

--- build.scm
+++ build.scm
@@ -1,40 +1,47 @@
+;; Load SXML tree transformation library
 (load "SXML-tree-trans.scm")
 
-
 ;; Load content definitions from the templates directory
-(load "page1.sxslt")
-(load "page2.sxslt")
-(load "page3.sxslt")
+(define content-definitions
+  (list "page1.sxslt" "page2.sxslt" "page3.sxslt"))
+(for-each load content-definitions)
 
 ;; Load XSLT templates from the templates directory
-(load "index.sxslt")
-(load "sections.sxslt")
-(load "footer.sxslt")
-(load "sections.sxslt")
+(define xslt-templates
+  (list "index.sxslt" "sections.sxslt" "footer.sxslt" "sections.sxslt"))
+(for-each load xslt-templates)
 
 ;; Define entag function
 (define (entag tag elems)
   (string-append "<" (symbol->string tag) ">"
-                 (apply string-append
-                        (map (lambda (e)
-                               (if (string? e)
-                                   e
-                                   (symbol->string e))) elems))
+                 (string-join (map (lambda (e)
+                                      (if (string? e)
+                                          e
+                                          (symbol->string e)))
+                                    elems)
+                               "")
                  "</" (symbol->string tag) ">"))
+(define (string-join lst sep)
+  (fold (lambda (x y) (string-append x sep y)) (car lst) (cdr lst)))
+;; Define enattr function
+(define (enattr attr-key value)
+  (string-append (symbol->string attr-key) "=\"" value "\""))
+(define (string-join lst sep)
+  (define (loop lst result)
+    (if (null? lst)
+        result
+        (loop (cdr lst) (string-append result sep (car lst)))))
+  (if (null? lst)
+      ""
+      (loop (cdr lst) (car lst))))
 
 ;; Define universal-conversion-rules
 (define universal-conversion-rules
   `((^
-      ((*DEFAULT*       ; local override for attributes
-        . ,(lambda (attr-key . value) (enattr attr-key value))))
-      . ,(lambda (trigger . value) (cons '^ value)))
+     ((*DEFAULT* . ,(lambda (attr-key . value) (enattr attr-key value))))
+     . ,(lambda (trigger . value) (cons '^ value)))
     (*DEFAULT* . ,(lambda (tag . elems) (entag tag elems)))
-    (*TEXT* . ,(lambda (trigger str) 
-                 (if (string? str) (string->goodHTML str) str)))
-    (n_        ; a non-breaking space
-     . ,(lambda (tag . elems)
-          (cons "&nbsp;" elems)))))
-
+    (*TEXT* . ,(lambda (trigger str) str))))
 
 ;; Define string-contains function
 (define (string-contains str substr)
@@ -66,30 +73,28 @@
   (with-output-to-file (string-append "output/" filename)
     (lambda ()
       (ssax:output-xml
-       `(html
+       `(html 
          (head (title ,title))
          (body
           (h1 ,title)
           ,@content))))))
 
+   
 ;; Define function to generate the index page in the output directory
 (define (generate-index)
   (with-output-to-file "output/index.html"
     (lambda ()
-      (ssax:output-xml
-       `(html
-         (head (title "Index"))
-         (body
-          (h1 "Index of Pages")
-          (ul
-           (li (a (@ (href "page1.html")) "Page 1"))
-           (li (a (@ (href "page2.html")) "Page 2"))
-           (li (a (@ (href "page3.html")) "Page 3")))))))))
+      (display  "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\"><body><h1>Index of Pages</h1><ul>")
+      (display "<li><a href=\"page1.html\">Page 1</a></li>")
+      (display "<li><a href=\"page2.html\">Page 2</a></li>")
+      (display "<li><a href=\"page3.html\">Page 3</a></li>")
+      (display "</ul></body></html>"))))
+  
 
-;; Generate the individual pages
+  ;; Generate the individual pages
 (generate-page page1-content "Page 1" "page1.html")
 (generate-page page2-content "Page 2" "page2.html")
 (generate-page page3-content "Page 3" "page3.html")
-
+ 
 ;; Generate the index page
 (generate-index)
